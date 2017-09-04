48da8f421c10dfc9b28673745c28af9aced74aa6
i.segodin <igor.segodin@gmail.com>
2017-09-04 23:43:57 +0300

    Initial commit

---
+++ client/desktop-client/src/main/java/org/isegodin/simple/messenger/client/desktop/App.java
@@ -0,0 +1,10 @@
+package org.isegodin.simple.messenger.client.desktop;
+
+/**
+ * @author isegodin
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello desktop client");
+    }
+}
